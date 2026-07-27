69fb415a40e66ddb506f3b998b7d8de3ed4480e5
muhammadali8303 <muhammadaliqaxxorov92@gmail.com>
2026-07-27 22:45:10 +0500

    first commit

---
+++ sctipt.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 20, // Kartochkalar orasidagi masofa
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1, // Mobil telefonda 1 ta kartochka
+            nav: false
+        },
+        600: {
+            items: 2, // Planshetda 2 ta kartochka
+            nav: false
+        },
+        1000: {
+            items: 4, // Kompyuterda 4 ta kartochka
+            nav: true,
+            loop: false
+        }
+    }
+});
